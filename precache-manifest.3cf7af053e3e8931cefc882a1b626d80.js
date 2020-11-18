cc4ec6045a4b9ceb1e427df9ae4f094c72e4665b
Cesar Blasco <cesarblasco50@hotmail.com>
2020-11-18 01:14:15 -0300

    Updates

---
+++ precache-manifest.3cf7af053e3e8931cefc882a1b626d80.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655641d2c882afdb447387388f6ce328",
+    "url": "/react-crypto/compare/index.html"
+  },
+  {
+    "revision": "53ad9be49f681583b694",
+    "url": "/react-crypto/compare/static/css/main.d67871d0.chunk.css"
+  },
+  {
+    "revision": "763299b3c464b5123ef1",
+    "url": "/react-crypto/compare/static/js/2.1cf174be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-crypto/compare/static/js/2.1cf174be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ad9be49f681583b694",
+    "url": "/react-crypto/compare/static/js/main.5ed70c82.chunk.js"
+  },
+  {
+    "revision": "4b319a2e8b8f5847b995",
+    "url": "/react-crypto/compare/static/js/runtime-main.21900672.js"
+  },
+  {
+    "revision": "f555bdcf7e814b3f8bbcd21c405d5252",
+    "url": "/react-crypto/compare/static/media/not-sorted.f555bdcf.svg"
+  },
+  {
+    "revision": "3a5f2344c363f163317b4c063497fac9",
+    "url": "/react-crypto/compare/static/media/sort-ascent.3a5f2344.svg"
+  },
+  {
+    "revision": "ca82bc743710e8f84904d035d92bf5cd",
+    "url": "/react-crypto/compare/static/media/sort-descent.ca82bc74.svg"
+  },
+  {
+    "revision": "5947ac02cb7f00f24b7739afa36cc5e3",
+    "url": "/react-crypto/compare/static/media/spinner.5947ac02.svg"
+  }
+]);
